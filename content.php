30a8c55880a24994b679228f9664d9a6084e8e36
ReD <lmadredl@hotmail.com>
2013-06-20 01:37:40 +0900

    fix some bugs
    - no link when not logged in
    - clamp and some other issues on firefox

--- content.php
+++ content.php
@@ -10,7 +10,12 @@
 
 	<article id="post-<?php the_ID(); ?>" <?php post_class('post-item'); ?>>
 		<div class="post-item_inr">
-			<a href="<?php the_permalink(); ?>" class="link_area <?php if(has_post_thumbnail()) echo 'wthumb'; ?>">
+			<?php if( is_user_logged_in() ) : ?>
+				<a href="<?php the_permalink(); ?>" class="link_area <?php if(has_post_thumbnail()) echo 'wthumb'; ?>">
+			<?php else : ?>
+				<div class="link_area <?php if(has_post_thumbnail()) echo 'wthumb'; ?>">
+			<?php endif; ?>
+			
 				<?php if ( is_sticky() && is_home() && ! is_paged() ) : ?>
 				<div class="featured-post">
 					<?php _e( 'Featured post', 'twentytwelve' ); ?>
@@ -32,17 +37,23 @@
 
 				<div class="entry-content">
 					<div class="entry-header">
-
 						<h1 class="entry-title"><?php echo $post->post_title; ?></h1>
 					</div><!-- .entry-header -->
 
 					<?php //the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?>
-					<?php the_excerpt(); ?>
+					<?php if( is_user_logged_in() ) the_excerpt(); ?>
 					<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'twentytwelve' ), 'after' => '</div>' ) ); ?>
 
 				</div><!-- .entry-content -->
 				<?php //endif; ?>
-			</a>
+			
+			<?php
+				if( is_user_logged_in() ) {
+					echo '</a>';
+				} else {
+					echo '</div>';
+				}
+			?>
 
 			<p class="entry-meta">
 				<?php twentytwelve_entry_meta(); ?>
